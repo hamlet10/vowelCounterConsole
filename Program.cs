123cef32ffca1718c4a1d4c3bcca15cfcbac0614
hamlet10 <hpsanchez@live.com>
2021-09-16 22:03:28 -0400

    imporint and using my lib

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using vowelCounter;
+
+namespace vowelCounterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = vowelCounter.vowelCounter.Counter("Hamlet tiene una A");
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+        }
+    }
+}
